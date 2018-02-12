bf542e4a1836c3b8b3f9618e73b30f6cc57a1fbd
Ramon Felix <ramonfe@gmail.com>
2018-02-11 20:33:10 -0800

    add table v 0.1
    futuros updates

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 $(document).ready(function (){
+var options = {
+		valueNames: ["material", "quantity", "price"]
+	},
+	documentTable = new List("mdl-table", options);
+
+$($("th.sort")[0]).trigger("click", function() {
+	console.log("clicked");
+});
+
+$("input.search").on("keyup", function(e) {
+	if (e.keyCode === 27) {
+		$(e.currentTarget).val("");
+		documentTable.search("");
+	}
+});
 getList();
 });
 
